d42892534750937fd0d34e2f9ef5a0650169c75a
mcyau17 <tommyyau76@gmail.com>
2023-04-25 16:22:48 +0100

    project upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'test',
+    loadChildren: () => import('./test/test.module').then( m => m.TestPageModule)
   },
+
 ];
 
 @NgModule({
